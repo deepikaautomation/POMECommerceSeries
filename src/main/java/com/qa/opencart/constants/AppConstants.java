99473896271ed4b61ae51b7d457d0954f67ff2dd
Sivanandan <mahesh.sivanandan@capgemini.com>
2025-01-29 20:29:59 -0500

    Appconstants corrected on login spell

--- src/main/java/com/qa/opencart/constants/AppConstants.java
+++ src/main/java/com/qa/opencart/constants/AppConstants.java
@@ -9,7 +9,7 @@ public class AppConstants {
 	public static final int DEFAULT_SHORT_TIME_OUT=5;
 	public static final int DEFAULT_LONG_TIME_OUT=20;
 	
-	public static final String LOGIN_PAGE_TITLE_VALUE="Account Login11";
+	public static final String LOGIN_PAGE_TITLE_VALUE="Account Login";
 	public static final String LOGIN_PAGE_URL_FRACTION_VALUE="route=account/login";
 	
 	public static final String ACCOUNTS_PAGE_TITLE_VALUE="My Account";
@@ -17,7 +17,7 @@ public class AppConstants {
 	public static final int ACCOUNTS_PAGE_HEADERS_COUNT=4;
 	
 	public static final List<String> EXPECTED_ACCOUNT_PAGE_HEADERS_LIST=Arrays.asList("My Account","My Orders",
-			     															 "My Affiliate Account","Newsletter","Deepika");
+			     															 "My Affiliate Account","Newsletter");
 	
 	public static final String SEARCH_PAGE_TITLE_VALUE="Search - ";
 	public static final String SEARCH_PAGE_URL_FRACTION_VALUE="search&search=";
